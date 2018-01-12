95df59419a1721eb502919d30f7e3c0af08da0af
Joakim Granbohm <joakim@adlaunch.com>
2018-01-12 10:49:18 +0200

    Improve boostrap script for different environments and backups

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dotfiles
-My dotfiles
+Very much WIP
+
+# Usage
+1. Git clone this to ~/dotfiles
+2. Run: ./bootsrap.sh [osx / ubuntu] 
+
+Running "./bootsrap.sh [environment]" will create backups from existing dotfiles and replace them with symlinks to these ones.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,55 @@
-files=".vimrc .zshrc"
-dir=~/dotfiles
-echo "Creating symlinks for dotfiles"
+#!/bin/bash
 
-for file in $files; do
+osxfiles=".zshrc"
+ubuntufiles=""
+sharedfiles=".vimrc"
+
+osxdir=~/dotfiles/osx
+ubuntudir=~/dotfiles/ubuntu
+shareddir=~/dotfiles/shared
+backupdir=~/dotfiles_backup
+
+if [ "$#" -ne 1 ]; then
+  echo "Please provide target system: osx or ubuntu"
+  exit 1
+fi
+
+echo "Setting up dotfiles for: $1"
+echo "-----------------------------------"
+
+if [ ! -d "$backupdir" ]; then
+  echo "Backup directory does not exist, creating $backupdir"
+  mkdir $backupdir
+fi
+
+echo "Creating symlinks for shared dotfiles"
+
+for file in $sharedfiles; do
   echo "Processing: $file"
-  ln -s $dir/$file ~/$file 
+ 
+  if [ -f ~/$file ]; then
+    echo "File $file exists, creating a backup."
+    cp ~/$file $backupdir/backup"$file"  
+    rm ~/$file
+  fi
+
+  ln -s $shareddir/$file ~/$file 
 done
+
+echo "Succefully created shared dotfiles symlinks"
+
+if [ "$1" == "osx" ]; then
+  echo "Creating symlinks for OSX"
+  
+  for file in $osxfiles; do
+    echo "Processing: $file"
+
+    if [ -f ~/$file ]; then
+      echo "File $file exists, creating a backup."
+      cp ~/$file $backupdir/backup"$file"  
+      rm ~/$file
+    fi
+
+    ln -s $osxdir/$file ~/$file 
+  done
+fi
